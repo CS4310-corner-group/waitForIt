84266d16d8bdb7b02a244705fd5ceb2c4077530f
jusko1505 <justinkousa@gmaillcom>
2023-03-11 12:41:20 -0800

    testing listing priority

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -81,7 +81,7 @@ ProcessList::Result ProcessList::exec()
                 char line[128];
                 snprintf(line, sizeof(line),
                         "%3d %7d %1d %4d %5d %10s %32s\r\n",
-                        pid, info.kernelState.parent, /* NEED TO DISPLAY PRIORITY INT SOMEHOW*/,
+                        pid, info.kernelState.parent, info.kernelState.priority,/* NEED TO DISPLAY PRIORITY INT SOMEHOW*/,
                         0, 0, *info.textState, *info.command);
                 out << line;
             }   
