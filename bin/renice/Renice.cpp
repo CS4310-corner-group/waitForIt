732adee153b6e08f4a7b674ae2ad975192405bb4
jusko1505 <justinkousa@gmaillcom>
2023-03-23 22:01:44 -0700

    hoepfullye

--- bin/renice/Renice.cpp
+++ bin/renice/Renice.cpp
@@ -1,31 +1,14 @@
-/*
- * Copyright (C) 2015 Niek Linnenbank
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-
+#include <sys/renice.h>
+#include "sys/renice.h"
 #include <stdio.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string.h>
 #include <unistd.h>
 #include "Renice.h"
-#include "sys/renice.h"
-#include <sys/renice.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+
 
 Renice::Renice(int argc, char **argv)
     : POSIXApplication(argc, argv)
@@ -50,12 +33,7 @@ Renice::Result Renice::exec()
         int priority = atoi(arguments().get("PRIORITY"));
         ProcessID PID = atoi(arguments().get("PID"));
         out << PID;
-    /*
-     if (modifypriority(PID, nullInt, priority) == -1){
-        ERROR("Something went wrong!");
-    };
-    */
-    modprio(PID, 0, 0, priority);
+        modprio(PID, 0, 0, priority);
     }
     
     
--- lib/libposix/sys/renice.h
+++ lib/libposix/sys/renice.h
@@ -1,42 +1,9 @@
-/*
- * Copyright (C) 2009 Niek Linnenbank
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef __LIBPOSIX_RENICE_H
 #define __LIBPOSIX_RENICE_H
 
 #include <Macros.h>
 #include "types.h"
 
-/**
- * @addtogroup lib
- * @{
- *
- * @addtogroup libposix
- * @{
- */
-
-/**
- * Returns the exit status of the child process
- *
- * @param st Contains the stat_loc value from renice()
- *
- * @return Exit status of the child process
- */
-#define WEXITSTATUS(st) (st)
 
 /**
  * @brief Change the priority of a process
@@ -45,8 +12,8 @@
  * @param pid Process ID of process to modify.
  * @param stat_loc Points to an integer for storing the exit status.
  * @param priority What priority to update to.
- *
- * @return Process ID of the child on success or -1 on error
+ * @param options Options
+ * @return Modify priority
  */
 extern C pid_t modprio(pid_t pid, int *stat_loc, int options, int priority);
 
