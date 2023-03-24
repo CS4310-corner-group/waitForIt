a94143a0905a231f53b97733f4bba3bab3584164
jusko1505 <justinkousa@gmaillcom>
2023-03-23 20:56:58 -0700

    change header file

--- bin/renice/Renice.h
+++ bin/renice/Renice.h
@@ -1,22 +1,6 @@
-/*
- * Copyright (C) 2015 Niek Linnenbank
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
-#ifndef __BIN_PS_PROCESSLIST_H
-#define __BIN_PS_PROCESSLIST_H
+#ifndef __BIN_WAIT_WAIT_H
+#define __WAIT_WAIT_H
 
 #include <POSIXApplication.h>
 
@@ -26,7 +10,7 @@
  */
 
 /**
- * Change priority of running processes.
+ * Stop executing for some time.
  */
 class Renice : public POSIXApplication
 {
@@ -41,6 +25,11 @@ class Renice : public POSIXApplication
     Renice(int argc, char **argv);
 
     /**
+     * Destructor
+     */
+    virtual ~Renice();
+
+    /**
      * Execute the application.
      *
      * @return Result code
@@ -52,4 +41,4 @@ class Renice : public POSIXApplication
  * @}
  */
 
-#endif /* __BIN_RENICE_RENICE_H */
+#endif /* __BIN_WAIT_WAIT_H */
